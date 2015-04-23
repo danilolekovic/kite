ae0f1c2df230ab507027ac711ef4ecd4f9daa919
danilolekovic <danilo@lekovic.ca>
2015-04-22 18:15:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# kite
+
+A super-small, portable, non-dependant JavaScript framework that allows for HTML in seperate files to be plotted anywhere.
+
+## features
+
+  * Only have a kilobyte (`503b`)!
+  * Fast!
+  * Error handling!
+
+## example
+
+**index.html:**
+```html
+<kite src="test.html"></kite>
+```
+
+**test.html:**
+```html
+<p>Hello world!</p>
+```
+
+## get it now
+
+Simply add this tag to the bottom of your HTML file.
+
+```js
+<script src="http://danilo.lekoits.com/kite/kite.min.js"></script>
+```
+
+## todo
+
+  * Multiple files in one tag!
+
+## license
+
+This project is licensed under the permissive [MIT license](https://raw.githubusercontent.com/danilolekovic/kite/master/LICENSE).
---
+++ src/kite.js
@@ -0,0 +1,26 @@
+var elements = document.getElementsByTagName("kite");
+
+function httpGet(address) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', address, false);
+  xhr.send(null);
+
+  if (xhr.status === 200) {
+    return xhr.responseText;
+  } else {
+    throw "kite: Could not fetch file: " + address;
+  }
+}
+
+for (var i = 0; i < elements.length; i++) {
+  var src = "";
+
+  if (elements[i].hasAttribute("src")) {
+    src += httpGet(elements[i].getAttribute("src"));
+    var htmlNode = document.createElement('span');
+    htmlNode.innerHTML = src;
+    elements[i].parentNode.replaceChild(htmlNode, elements[i]);
+  } else {
+    throw "kite: " + elements[i] + " has no src location.";
+  }
+}
